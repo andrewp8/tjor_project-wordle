c9bd84cf72c29810636c95969a59317223bff8f5
Andrew Pham <minhanh.vietpham@gmail.com>
2024-07-24 23:00:04 -0500

    finished Exercise 1: GuessInput

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,7 +9,20 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("");
+  const [value, setValue] = React.useState("")
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newGuess = event.target.elements[0].value.toUpperCase();
+    setGuess(newGuess);
+    setValue("");
+    console.info({ guess: newGuess});
+  }
+  return <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+    <label htmlFor="guess-input">Enter guess2:</label>
+    <input id="guess-input" type="text" value={value} onChange = {event=>setValue(event.target.value)} pattern="[A-Za-z]{5}"/>
+  </form>;
 }
 
 export default Game;
--- src/index.js
+++ src/index.js
@@ -6,5 +6,14 @@ import App from './components/App';
 import './reset.css';
 import './styles.css';
 
+
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+      const NextApp = require('./components/App').default;
+      root.render(NextApp);
+  });
+}
