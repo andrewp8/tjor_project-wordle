cfee3be083c9ece5cb92584462654c68988c4792
Andrew Pham <minhanh.vietpham@gmail.com>
2024-07-24 23:14:12 -0500

    finished Exercise 2: Keeping track of guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,19 +8,19 @@ const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-function Game() {
-  const [guess, setGuess] = React.useState("");
+function Game({guesses, setGuesses}) {
   const [value, setValue] = React.useState("")
   
   const handleSubmit = (event) => {
     event.preventDefault();
     const newGuess = event.target.elements[0].value.toUpperCase();
-    setGuess(newGuess);
+    const newGuesses = [...guesses, newGuess]
+    setGuesses(newGuesses);
     setValue("");
     console.info({ guess: newGuess});
   }
   return <form className="guess-input-wrapper" onSubmit={handleSubmit}>
-    <label htmlFor="guess-input">Enter guess2:</label>
+    <label htmlFor="guess-input">Enter guess:</label>
     <input id="guess-input" type="text" value={value} onChange = {event=>setValue(event.target.value)} pattern="[A-Za-z]{5}"/>
   </form>;
 }
---
+++ src/components/Guesses/Guesses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Guesses({guesses}) {
+  return <div className="guess-results">
+    {guesses.map(guess =>(
+      <p className="guess" key={crypto.randomUUID()}>{guess}</p>
+    ))}
+</div>
+}
+
+export default Guesses;
