36f47d2a7cef631d81b4634ef3126652b18a0a34
Andrew Pham <minhanh.vietpham@gmail.com>
2024-07-31 18:13:31 -0500

    Working on E3

--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { range } from '../../utils.js'
 
-function Guesses({guesses}) {
-  return <div className="guess-results">
-    {guesses.map(guess =>(
-      <p className="guess" key={crypto.randomUUID()}>{guess}</p>
+/* {guesses.map(guess => (
+  <p className="guess" key={crypto.randomUUID()}>
+    {guess.split("").map(letter => (
+      <span className="cell" key={crypto.randomUUID()}>{letter}</span>
     ))}
-</div>
+  </p>
+))} */
+function Guesses({ guesses }) {
+  return <div className="guess-results">
+    {range(6).map(guess =>(
+      <p className="guess" key={crypto.randomUUID()}>
+        {range(5).map(letter => (
+          <span className="cell">{letter}</span>
+        ))}
+      </p>
+    ))}
+  </div >
 }
 
 export default Guesses;
