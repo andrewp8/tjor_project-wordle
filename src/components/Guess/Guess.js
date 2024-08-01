b339565c193c9f5db2bb18d3f879df61951c41d2
Andrew Pham <minhanh.vietpham@gmail.com>
2024-08-01 12:48:03 -0500

    [My approach], solving directly in the <Guess/>, a little hard to read

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import { range } from '../../utils';
-function Guess({ value }) {
+import { checkGuess } from "../../game-helpers";
+
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
   return <>
     <p className="guess">
       {range(5).map(index =>
-        <span key={index} className="cell">
+        <span key={index} className={`cell ${value ? result[index].status : ""}`}>
           {value ? value[index] : undefined}
           </span>
       )}
