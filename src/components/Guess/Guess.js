d51db658e5beb87e8ce673d459f52ef83aad5127
Andrew Pham <minhanh.vietpham@gmail.com>
2024-08-01 16:10:30 -0500

    Josh approach

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,14 +3,22 @@ import React from 'react';
 import { range } from '../../utils';
 import { checkGuess } from "../../game-helpers";
 
+function Cell({letter, status}) {
+  const className = status ? `cell ${status}` : "cell"
+  return <span className={className}>
+    {letter}
+  </span>
+}
+
 function Guess({ value, answer }) {
   const result = checkGuess(value, answer);
   return <>
     <p className="guess">
       {range(5).map(index =>
-        <span key={index} className={`cell ${value ? result[index].status : ""}`}>
-          {value ? value[index] : undefined}
-          </span>
+        <Cell key={index}
+          letter={result ? result[index].letter : undefined} 
+          status={result ? result[index].status : undefined} 
+        />
       )}
     </p>
   </>;
